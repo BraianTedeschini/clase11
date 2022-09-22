cfb0420c22d61c40a4408701116c4a9d1bdad6a7
BraianTedeschini <107695531+BraianTedeschini@users.noreply.github.com>
2022-09-21 23:26:29 -0300

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "Nicolau", tipo: "buzo", cantidad: 1, desc: "Pessoa capaz de resolver todos os tipos de problemas na estrada", precio: 1200, talle: "7", img: './img/buzo.jpg'},
+    {id: 7, nombre: "Carlos", tipo: "remera", cantidad: 1, desc: "Um motorista capaz de chegar em tempo hábil, mantendo a segurança do caminhão e da carga", precio: 500, talle: "8", img: './img/remera.jpg'},
+    {id: 13, nombre: "Roman", tipo: "camisa", cantidad: 1, desc: "Motorista mental e fisicamente preparado para longas viagens", precio: 900, talle: "9", img: './img/camisa.jpg'},
+    {id: 19, nombre: "Jeremias", tipo: "pantalon", cantidad: 1, desc: "Motorista de caminhão preparado e treinado para fazer viagens de curta distância", precio: 1600, talle: "8", img: './img/pantalon.jpg'},
+    ]
